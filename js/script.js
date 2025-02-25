b23800addd06c6662614f1e93aa1a608d5d27247
wefun0454 <1027345388@qq.com>
2025-02-25 15:56:09 +0800

    Site updated: 2025-02-25 15:56:09

--- js/script.js
+++ js/script.js
@@ -1,138 +1,134 @@
-(function($){
-  // Search
-  var $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
+(function () {
+  // A Simple EventListener
+  [Element, Document, Window].forEach((target) => {
+    target.prototype._addEventListener = target.prototype.addEventListener;
+    target.prototype._removeEventListener =
+      target.prototype.removeEventListener;
+    target.prototype.addEventListener = target.prototype.on = function (
+      name,
+      listener,
+      options
+    ) {
+      this.__listeners__ = this.__listeners__ || {};
+      this.__listeners__[name] = this.__listeners__[name] || [];
 
-  var startSearchAnim = function(){
-    isSearchAnim = true;
-  };
-
-  var stopSearchAnim = function(callback){
-    setTimeout(function(){
-      isSearchAnim = false;
-      callback && callback();
-    }, searchAnimDuration);
-  };
-
-  $('.nav-search-btn').on('click', function(){
-    if (isSearchAnim) return;
-
-    startSearchAnim();
-    $searchWrap.addClass('on');
-    stopSearchAnim(function(){
-      $('.search-form-input').focus();
-    });
-  });
-
-  $('.search-form-input').on('blur', function(){
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
-  });
-
-  // Share
-  $('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
-    e.stopPropagation();
-
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      title = $this.attr('data-title'),
-      offset = $this.offset();
-
-    if ($('#' + id).length){
-      var box = $('#' + id);
-
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
+      // Check if the listener is already added
+      for (let [l, o] of this.__listeners__[name]) {
+        if (l === listener && JSON.stringify(o) === JSON.stringify(options)) {
+          return this; // Listener is already added, do nothing
+        }
       }
+      this.__listeners__[name].push([listener, options]);
+      this._addEventListener(name, listener, options);
+      return this;
+    };
+    target.prototype.removeEventListener = target.prototype.off = function (
+      name,
+      listener,
+      options
+    ) {
+      if (!this.__listeners__ || !this.__listeners__[name]) {
+        return this;
+      }
+      if (!listener) {
+        // remove all event listeners
+        this.__listeners__[name].forEach(([listener, options]) => {
+          this.removeEventListener(name, listener, options);
+        });
+        delete this.__listeners__[name];
+        return this;
+      }
+      this._removeEventListener(name, listener, options);
+      this.__listeners__[name] = this.__listeners__[name].filter(
+        ([l, o]) =>
+          l !== listener || JSON.stringify(o) !== JSON.stringify(options)
+      );
+      if (this.__listeners__[name].length === 0) {
+        delete this.__listeners__[name];
+      }
+      return this;
+    };
+  });
+  // Simple Selector
+  window._$ = (selector) => document.querySelector(selector);
+  window._$$ = (selector) => document.querySelectorAll(selector);
+
+  // dark_mode
+  const themeButton = document.createElement('a');
+  themeButton.className = 'nav-icon dark-mode-btn';
+  _$('#sub-nav').append(themeButton);
+
+  const osMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  function setTheme(config) {
+    const isAuto = config === 'auto';
+    const isDark = config === 'true' || (isAuto && osMode);
+    
+    document.documentElement.setAttribute('data-theme', isDark ? 'dark' : null);
+    localStorage.setItem('dark_mode', config);
+    
+    themeButton.id = `nav-${
+      config === 'true' ? 'moon' : 
+      config === 'false' ? 'sun' : 
+      'circle-half-stroke'
+    }-btn`;
+    
+    document.body.dispatchEvent(new CustomEvent(`${isDark ? 'dark' : 'light'}-theme-set`));
+  }
+  const savedMode =
+    localStorage.getItem("dark_mode") ||
+    document.documentElement.getAttribute("data-theme-mode") ||
+    "auto";
+  setTheme(savedMode);
+
+  themeButton.addEventListener('click', () => {
+    const modes = ['auto', 'false', 'true'];
+    const nextMode = modes[(modes.indexOf(localStorage.getItem('dark_mode')) + 1) % 3];
+    setTheme(nextMode);
+  });
+
+  let oldScrollTop = 0;
+  document.addEventListener("scroll", () => {
+    let scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    const diffY = scrollTop - oldScrollTop;
+    window.diffY = diffY;
+    oldScrollTop = scrollTop;
+    if (diffY < 0) {
+      _$("#header-nav").classList.remove("header-nav-hidden");
     } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?text=' + encodeURIComponent(title) + '&url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"><span class="fa fa-twitter"></span></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"><span class="fa fa-facebook"></span></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"><span class="fa fa-pinterest"></span></a>',
-            '<a href="https://www.linkedin.com/shareArticle?mini=true&url=' + encodedUrl + '" class="article-share-linkedin" target="_blank" title="LinkedIn"><span class="fa fa-linkedin"></span></a>',
-          '</div>',
-        '</div>'
-      ].join('');
-
-      var box = $(html);
-
-      $('body').append(box);
+      _$("#header-nav").classList.add("header-nav-hidden");
     }
-
-    $('.article-share-box.on').hide();
-
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
-
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
   });
 
-  // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox') || $(this).parent().is('a')) return;
-
-      var alt = this.alt;
-
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
-
-      $(this).wrap('<a href="' + this.src + '" data-fancybox=\"gallery\" data-caption="' + alt + '"></a>')
+  if (window.Pace) {
+    Pace.on("done", () => {
+      Pace.sources[0].elements = [];
     });
+  }
+})();
 
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
-    });
-  });
+var safeImport = async (url, integrity) => {
+  if (!integrity) {
+    return import(url);
+  }
+  const response = await fetch(url);
+  const moduleContent = await response.text();
 
-  if ($.fancybox){
-    $('.fancybox').fancybox();
+  const actualHash = await crypto.subtle.digest(
+    "SHA-384",
+    new TextEncoder().encode(moduleContent)
+  );
+  const hashBase64 =
+    "sha384-" + btoa(String.fromCharCode(...new Uint8Array(actualHash)));
+
+  if (hashBase64 !== integrity) {
+    throw new Error(`Integrity check failed for ${url}`);
   }
 
-  // Mobile nav
-  var $container = $('#container'),
-    isMobileNavAnim = false,
-    mobileNavAnimDuration = 200;
+  const blob = new Blob([moduleContent], { type: "application/javascript" });
+  const blobUrl = URL.createObjectURL(blob);
+  const module = await import(blobUrl);
+  URL.revokeObjectURL(blobUrl);
 
-  var startMobileNavAnim = function(){
-    isMobileNavAnim = true;
-  };
-
-  var stopMobileNavAnim = function(){
-    setTimeout(function(){
-      isMobileNavAnim = false;
-    }, mobileNavAnimDuration);
-  }
-
-  $('#main-nav-toggle').on('click', function(){
-    if (isMobileNavAnim) return;
-
-    startMobileNavAnim();
-    $container.toggleClass('mobile-nav-on');
-    stopMobileNavAnim();
-  });
-
-  $('#wrap').on('click', function(){
-    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
-
-    $container.removeClass('mobile-nav-on');
-  });
-})(jQuery);
+  return module;
+};
