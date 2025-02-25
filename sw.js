6b57b8f94d38222c62210adbf3242a8a5c36c2a7
wefun0454 <1027345388@qq.com>
2025-02-25 16:15:54 +0800

    Site updated: 2025-02-25 16:15:53

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1740470155758";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1740471340953";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
